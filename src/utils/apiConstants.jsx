cfc29e4b8c74ba2087d406fa44d68b773101fee7
Juanca <juancasan17@gmail.com>
2018-11-12 16:26:29 -0600

    prepare refactor

--- src/utils/apiConstants.jsx
+++
@@ -1,3 +0,0 @@
-export const URL = "https://kremlin-api.herokuapp.com/api/";
-export const SAMPLE_WEATHER =
-  "http://api.openweathermap.org/data/2.5/weather?q=Guadalajara,mx&units=metric&APPID=637e3c746f310a2d62516cc91508a09f";
